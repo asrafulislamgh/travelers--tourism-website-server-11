60e7d828a2fe6e3ca707d2d3ed58f46e0e8735ba
asrafulislamgh <gdmamun@yahoo.com>
2021-10-29 16:29:38 +0600

    server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { MongoClient } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 // MidlleWare
 app.use(cors());
